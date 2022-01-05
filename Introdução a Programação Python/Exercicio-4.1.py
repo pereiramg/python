16325fec554104e8bcd1ec4c3b29257a96cd8b29
Marcelo Galdino Pereira <pereiramgster@gmail.com>
2022-01-04 21:26:58 -0300

    Exercicio-4.1.py

---
+++ Introdução a Programação Python/Exercicio-4.1.py
@@ -0,0 +1,8 @@
+# Lê dois valores e imprime qual é o valor
+
+a = int(input("Primeiro valor: "))
+b = int(input("Segundo valor: "))
+if a > b :
+    print("O primeiro valor é o maior!")
+if b > a :
+    print("O segundo valor é maior!")
