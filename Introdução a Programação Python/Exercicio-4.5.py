bc3340883c38f4715f4f0f239217c2e038d3f2a4
Marcelo Galdino Pereira <pereiramgster@gmail.com>
2022-01-04 23:24:37 -0300

    Exercicio-4.5.py

---
+++ Introdução a Programação Python/Exercicio-4.5.py
@@ -0,0 +1,8 @@
+# Carro novo ou velho, dependendo da idade
+
+idade = int(input("Digite a idade de seu carro: "))
+
+if idade <= 3 :
+    print("Seu carro é novo")
+else :
+    print("Seu carro é velho")
