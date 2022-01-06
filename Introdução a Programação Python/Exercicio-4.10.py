e14e6711d28e89198ed9c04da82493546fa7165d
Marcelo Galdino Pereira <pereiramgster@gmail.com>
2022-01-06 20:52:33 -0300

    Exercicio-4.10.py

--- Introdução a Programação Python/Exercicio-4.10.py
+++ Introdução a Programação Python/Exercicio-4.10.py
@@ -12,3 +12,32 @@
 
 # industrial - até 5000 - 0,55
 # industrial - acima de 5000 - 0,60
+
+consumo = int(input("Qual foi a quantidade de Kwh consumido? "))
+print("Qual o tipo de instalação?")
+print("1 - para residencia")
+print("2 - para indústrias")
+print("3 - para comércios")
+instalacao = int(input("Escolha as opções de 1 a 3 "))
+
+
+if instalacao == 1:
+    if consumo > 500:
+        pagar = consumo * 0.65
+    else:
+        pagar = consumo * 0.40
+elif instalacao == 2:
+    if consumo > 5000:
+        pagar = consumo * 0.60
+    else:
+        pagar = consumo * 0.55
+elif instalacao == 3:
+    if consumo > 1000:
+        pagar = consumo * 0.60
+    else:
+        pagar = consumo * 0.55
+else:
+    print("Valor invalido de entrada")
+    pagar = 0
+
+print(f"O valor a pagar na sua conta de luz é R${pagar:4.2f}")
