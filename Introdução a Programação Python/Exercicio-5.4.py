fc29d79c9023bd39381801b953953fc2cd6ff170
Marcelo Galdino Pereira <pereiramgster@gmail.com>
2022-01-11 21:02:16 -0300

    Exercicio-5.4.py

---
+++ Introdução a Programação Python/Exercicio-5.4.py
@@ -0,0 +1,8 @@
+# Modifique o programa anterior para imprimir de 1 até o número digitado pelo usuário, 
+# mas, dessa vez, apenas os números ímpares
+
+fim = int(input("Digite o último número a imprirmir: "))
+x = 1
+while x <= fim:
+    print(x)
+    x += 2
