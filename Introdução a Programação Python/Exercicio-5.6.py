0f037527c8adfd77079fa71fa0a432379c3463e8
Marcelo Galdino Pereira <pereiramgster@gmail.com>
2022-01-20 08:21:37 -0300

    Exercicio-5.6.py

---
+++ Introdução a Programação Python/Exercicio-5.6.py
@@ -0,0 +1,8 @@
+# Altere o programa anterior para exibir os resultados no mesmo formato
+# de uma tabuada: 2x1 = 2....
+
+tabuada = int(input("Tabuada de: "))
+x = 1
+while x <= 10:
+    print(tabuada * x)
+    x += 1
