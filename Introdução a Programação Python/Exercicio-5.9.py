4f3877bc56523bb91e37198b7afa272bd685a110
Marcelo Galdino Pereira <pereiramgster@gmail.com>
2022-01-20 20:13:52 -0300

    Exercicio-5.9.py

--- Introdução a Programação Python/Exercicio-5.9.py
+++ Introdução a Programação Python/Exercicio-5.9.py
@@ -1,6 +1,19 @@
-# Escreva um programa que leia dois numeros. Imprima a divisão inteira do 
-# primeiro pelo segundo, assim como o resto da divisão. Utilize apenas os
-# operadores de soma e subtração para calcular o resultado. Lembre-se de
-# que podemos entender o quociente da divisão de dois números como a 
+# Escreva um programa que leia dois numeros. 
+# Imprima a divisão inteira do primeiro pelo segundo, assim como o resto da divisão. 
+# Utilize apenas os operadores de soma e subtração para calcular o resultado. 
+# Lembre-se de que podemos entender o quociente da divisão de dois números como a 
 # quantidade de vezes que podemos retirar o divisor do dividendo.
 # Logo, 20 / 4 = 5, uma vez que podemos subtrair 4 cinco vezes de 20
+
+
+numero1 = int(input("Insira o primeiro valor: "))
+numero2 = int(input("Insira o segundo valor "))
+
+x = 0
+sobra = numero1
+
+while sobra >= numero2 :
+    sobra -= numero2
+    x += 1
+
+print(f"O valor da divisão do {numero1} / {numero2} é: {x}, e sua sobra é {sobra}")
