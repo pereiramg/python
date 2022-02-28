4cf92674b8a6c1c889ccd54bbd093e862e8e1745
Marcelo Galdino Pereira <pereiramgster@gmail.com>
2022-02-28 14:18:31 -0300

    Exercicios-5-21.py

---
+++ Introdução a Programação Python/Exercicios-5-21.py
@@ -0,0 +1,28 @@
+# Execute o programa com os seguintes valores
+# 501, 745, 384, 2, 7 e 1
+
+from ast import While
+
+valor = 1
+while valor > 0:
+    valor = int(input("Digite o valor a pagar: "))
+    cedulas = 0
+    atual = 50
+    apagar = valor
+    while valor > 0 :
+        if atual <= apagar:
+            apagar -= atual
+            cedulas += 1
+        else:
+            print(f"{cedulas} cedula(s) de R${atual}")
+            if apagar == 0:
+                break
+            if atual == 50:
+                atual = 20
+            elif atual == 20:
+                atual = 10
+            elif atual == 10:
+                atual = 5
+            elif atual == 5:
+                atual = 1
+            cedulas = 0
